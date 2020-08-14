c23a77dd4a614d485b53fbac30c1911976c2954c
usernamedt <usernamedt@protonmail.ch>
2020-08-14 16:28:41 +0500

    Add more wal show tests

--- internal/wal_show_handler_test.go
+++ internal/wal_show_handler_test.go
@@ -1,10 +1,40 @@
 package internal_test
 
 import (
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/wal-g/wal-g/internal"
 	"testing"
 )
+
+// TestSegmentSequenceSearchMissingInRange verifies that FindMissingSegments searches for missing
+// segments only in range [minSegmentNo, maxSegmentNo]
+func TestSegmentSequenceSingleElement(t *testing.T) {
+	segmentNo := 5000
+	timelineId := uint32(1)
+
+	segmentSequence := internal.NewSegmentsSequence(timelineId, internal.WalSegmentNo(segmentNo))
+
+	missingSegments, err := segmentSequence.FindMissingSegments()
+	assert.NoError(t, err)
+
+	assert.Equal(t, 0, len(missingSegments))
+}
+
+// TestSegmentSequenceSearchMissingInRange verifies that FindMissingSegments searches for missing
+// segments only in range [minSegmentNo, maxSegmentNo]
+func TestSegmentSequenceNoMissingSegments(t *testing.T) {
+	minSegmentNo := 5000
+	maxSegmentNo := 10000
+	// no missing segments
+	missingSegments := make(map[internal.WalSegmentNo]bool)
+
+	foundMissing, err := testFindMissingElementsInSequence(t, minSegmentNo, maxSegmentNo, missingSegments)
+	assert.NoError(t, err)
+	
+	// check that there are no missing segments found
+	assert.True(t, len(foundMissing) == 0)
+}
 
 // TestSegmentSequenceSearchMissingInRange verifies that FindMissingSegments searches for missing
 // segments only in range [minSegmentNo, maxSegmentNo]
@@ -66,4 +96,32 @@ func testFindMissingElementsInSequence(t *testing.T, minSegmentNo, maxSegmentNo 
 	// check that FindMissingSegments() returned only unique elements
 	assert.True(t, len(missingSegments) == len(missingNumbersSet))
 	return missingNumbersSet, nil
+}
+
+func TestTimelineInfo(t *testing.T) {
+	timelineId := uint32(1)
+	segmentNo := internal.WalSegmentNo(500)
+	walSegment := internal.WalSegmentDescription{Number: segmentNo, Timeline: timelineId}
+	walSegmentFilename := walSegment.GetFileName()
+
+	walSegmentSequence := internal.NewSegmentsSequence(timelineId, segmentNo)
+	historyRecords := make([]*internal.TimelineHistoryRecord,0)
+
+	timelineInfo, err := internal.NewTimelineInfo(walSegmentSequence, historyRecords)
+	assert.NoError(t, err)
+
+	expectedTimelineInfo := internal.TimelineInfo{
+		Id: timelineId,
+		ParentId: 0,
+		SwitchPointLsn: 0,
+		StartSegment: walSegmentFilename,
+		EndSegment: walSegmentFilename,
+		SegmentsCount: 1,
+		MissingSegments: make([]string, 0),
+		Backups: nil,
+		SegmentRangeSize: 1,
+		Status: internal.TimelineOkStatus,
+	}
+
+	assert.True(t, cmp.Equal(expectedTimelineInfo, *timelineInfo))
 }
